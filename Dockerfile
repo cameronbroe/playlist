29b6d9d6ab1b87aa1115bbe5a5ee84e73e272f9b
Cameron Roe <cameron@cameronbroe.com>
2022-08-01 01:02:36 -0500

    Set up Gin with a basic route

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM golang:1.18.4 AS build
+
+ENV CGO_ENABLED=0
+
 WORKDIR /music.cameronbroe.com
-COPY . .
+COPY ./go.mod ./go.mod
+COPY ./go.sum ./go.sum
 RUN go mod download
+
+COPY . .
 RUN go build -o build/server cmd/server.go
 
 FROM scratch
--- cmd/server.go
+++ cmd/server.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type App struct {
+	server *gin.Engine
+}
+
+func buildApp() *App {
+	app := new(App)
+
+	app.server = gin.Default()
+
+	app.server.GET("/list", func(c *gin.Context) {
+		fmt.Println("Got a request to /list")
+	})
+
+	return app
+}
+
+func (app *App) Run() {
+	app.server.Run()
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	app := buildApp()
+
+	app.Run()
 }
