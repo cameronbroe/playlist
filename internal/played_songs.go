2766dce62522994fc5c1c662beeab8c835351702
Cameron Roe <cameron@cameronbroe.com>
2022-11-06 15:12:30 -0600

    Fix a typo

--- internal/played_songs.go
+++ internal/played_songs.go
@@ -6,6 +6,6 @@ type PlayedSong struct {
 	Artist     string `json:"artist_name"`
 	Album      string `json:"album_name"`
 	AppleUrl   string `json:"apple_url"`
-	SpotifyUrl string `json:"spoitfy_url"`
+	SpotifyUrl string `json:"spotify_url"`
 	YouTubeUrl string `json:"youtube_url"`
 }
